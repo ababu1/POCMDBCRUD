a7fbc13df7e4d25b5b60cec475acb840262d6d07
“ababu1” <ajay.babu.osv@fedex.com>
2023-03-15 21:55:38 -0500

    aggregation changes draft commented code

--- src/main/kotlin/com/poc/mongodb/db/repository/impl/InventoryLevelMongoTemplateRepoImpl.kt
+++ src/main/kotlin/com/poc/mongodb/db/repository/impl/InventoryLevelMongoTemplateRepoImpl.kt
@@ -1,6 +1,5 @@
 package com.poc.mongodb.db.repository.impl
 
-import com.poc.mongodb.db.model.InventoryItem
 import com.poc.mongodb.db.model.InventoryLevel
 import com.poc.mongodb.db.repository.InventoryLevelMongoTemplateRepo
 import com.poc.mongodb.db.request.FacilityResponseVO
@@ -10,11 +9,21 @@ import com.poc.mongodb.db.request.SetOrAdjust
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.Aggregation.group
+import org.springframework.data.mongodb.core.aggregation.Aggregation.match
+import org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation
+import org.springframework.data.mongodb.core.aggregation.Aggregation.project
+import org.springframework.data.mongodb.core.aggregation.Aggregation.sort
+import org.springframework.data.mongodb.core.aggregation.AggregationResults
+import org.springframework.data.mongodb.core.aggregation.SortOperation
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
 import org.springframework.stereotype.Repository
 import java.util.*
+
 
 @Repository
 class InventoryLevelMongoTemplateRepoImpl : InventoryLevelMongoTemplateRepo {
@@ -43,7 +52,11 @@ class InventoryLevelMongoTemplateRepoImpl : InventoryLevelMongoTemplateRepo {
          val query: Query = Query().addCriteria(Criteria.where("itemDetail.orgId").`is`(orgId))
                 .addCriteria(Criteria.where("itemDetail.sku").`in`(sku))
                 .addCriteria(Criteria.where("facilityId").`in`(facilityId))
+                .addCriteria(Criteria.where("count").`gt`(0))
+
          query.with(Sort.by(Sort.Direction.ASC, "facilityId"))
+
+
 
          var rs = mongoTemplate.find(query, InventoryLevel::class.java )
 
@@ -70,6 +83,31 @@ class InventoryLevelMongoTemplateRepoImpl : InventoryLevelMongoTemplateRepo {
             facilitySummary.add(FacilitySummaryVO(facilityId = facilityPrev, items=itemSummary))
         }
         facilityResponse.facilitySummary =facilitySummary
+
+
+//        val groupByfacilityId = group("facilityId")
+//            .addToSet("items").`as`("items")
+//            .sum("count").`as`("availableQuantity")
+//        val filterCount = match(Criteria("count").gt(0))
+//        val sortByFacilityId: SortOperation = sort(Sort.by(Sort.Direction.ASC, "facilityId"))
+//        val projectObject: ProjectionOperation = project( "items":[["$itemDetail.sku", "$count"]])
+//            .and("facilityId").previousOperation()
+
+//        val result: AggregationResults<FacilityResponseVO> = mongoTemplate.aggregate(Aggregation.newAggregation(
+//            filterCount,
+//            groupByfacilityId,
+//            projectObject
+//        ), InventoryLevel.class, FacilityResponseVO.class).getMappedResults()
+
+//        val aggregation: Aggregation = newAggregation(
+//            filterCount,
+//            groupByfacilityId,
+//            projectObject
+//        )
+//        val result: AggregationResults<FacilityResponseVO> = mongoTemplate.aggregate(
+//            aggregation, "inventory_level", FacilityResponseVO::class.java
+//        )
+
         return facilityResponse
         }
 
